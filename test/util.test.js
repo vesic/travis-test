ba4536cf9cdd1a32247a9b4a177ad16e6253c250
vesic <dusan.vesic@gmail.com>
2017-05-12 09:01:48 +0000

    Write some test add another util method

--- test/util.test.js
+++ test/util.test.js
@@ -6,4 +6,14 @@ describe('testing util.js', function() {
     it('should return string hello', function() {
         expect(util.hello()).to.equal('hello');
     })
+    
+    it('should return first letter of the string', function() {
+        let str = 'hello';
+        expect(util.returnFirstLetter(str)).to.equal('h');
+    })
+    
+    it('should be a string', function() {
+        let str = 'hello';
+        expect(util.returnFirstLetter(str)).to.be.a('number');
+    })
 })
